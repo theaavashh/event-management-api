aedad4cab8d7a47151833db995d925371d9587d7
thataavashh <theaavashhganesh0@gmail.com>
2024-03-14 20:47:25 +0545

    Refactor logerg file for mailing log

--- src/utilits/logger.utilis.ts
+++ src/utilits/logger.utilis.ts
@@ -4,19 +4,12 @@ const { timestamp,combine,prettyPrint } = format;
 
 const logger = createLogger({
 	level: "info",
-	format: combine(timestamp(),format.json(),	prettyPrint(),),
+	format: combine(timestamp(),format.json(),	prettyPrint()),
 	defaultMeta: { service: "user-service" },
 	transports: [
 		new transports.File({ filename: "log/mailing.log" }),
 	],
 });
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== "production") {
-	logger.add(new winston.transports.Console({
-		format: winston.format.simple(),
-	}));
-}
+
+export default logger;
